5cf57130f938fb14b9e7f4046d43d7cc30fa3fd5
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-05-08 12:17:01 +0100

    input location without style

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div className="c-navbar">
       <div className="c-navbar__title">Title</div>
       <div className="c-navbar__input">
-        <input className="c-navbar__imput--box" type="text" placeholder="...enter location" />
+        <form className="c-navbar__input--form" onSubmit={(e) => props.changeWeather(e)}>
+          <input className="c-navbar__imput--box" type="text" placeholder="...enter location" onChange={(e) => { props.changeRegion(e.target.value) }} />
+        </form>
       </div>
     </div>
   )
