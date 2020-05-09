4f7ad5f075502b507f49da4865f91dbfac4620c3
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-05-09 13:03:06 +0100

    style and constructor added

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Navbar(props) {
   return (
     <div className="c-navbar">
-      <div className="c-navbar__title">Title</div>
+      <div className="c-navbar__title">Weather App</div>
       <div className="c-navbar__input">
         <form className="c-navbar__input--form" onSubmit={(e) => props.changeWeather(e)}>
-          <input className="c-navbar__imput--box" type="text" placeholder="...enter location" onChange={(e) => { props.changeRegion(e.target.value) }} />
+          <input className="c-navbar__input--box" type="text" placeholder="enter location" onChange={(e) => { props.changeRegion(e.target.value) }} />
         </form>
       </div>
     </div>
